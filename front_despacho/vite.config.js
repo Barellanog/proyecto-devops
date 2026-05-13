b5290b6b7f445d78ec215ed606a6ea60551e42d0
barellanog <benj.arellano@duocuc.cl>
2026-05-13 19:04:25 -0400

    feat: add multi-stage Dockerfile and nginx proxy config for frontend

--- front_despacho/vite.config.js
+++ front_despacho/vite.config.js
@@ -1,15 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'https://qic534o8o0.execute-api.us-east-1.amazonaws.com',
+      '/api/v1/ventas': {
+        target: 'http://localhost:8080',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+      },
+      '/api/v1/despachos': {
+        target: 'http://localhost:8081',
+        changeOrigin: true,
       }
     }
   }
